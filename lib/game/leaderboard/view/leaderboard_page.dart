99252b0a2071e0bc78db8a9038ddc708469e5230
Hibatullah Fawwaz Hana <hiba.kudus@gmail.com>
2024-02-28 14:59:10 +0700

    feat: leaderboard

--- lib/game/leaderboard/view/leaderboard_page.dart
+++ lib/game/leaderboard/view/leaderboard_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:garda_green/audio/audio.dart';
 import 'package:garda_green/game/leaderboard/leaderboard.dart';
+import 'package:garda_green/gen/assets.gen.dart';
 import 'package:garda_green/l10n/l10n.dart';
 import 'package:garda_green/leaderboard/leaderboard.dart';
 import 'package:garda_green/utils/utils.dart';
@@ -69,31 +70,170 @@ class LeaderboardView extends StatelessWidget {
             context.read<AudioController>().changeMusic(Song.background);
           }
         },
-        child: Padding(
-          padding: EdgeInsets.symmetric(
-            horizontal:
-                context.isSmall ? 32 : MediaQuery.of(context).size.width / 4,
+        child: DecoratedBox(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              filterQuality: FilterQuality.high,
+              image: context.isSmall
+                  ? Assets.images.leaderboardMobile.provider()
+                  : Assets.images.leaderboardDesktop.provider(),
+              fit: context.isSmall ? BoxFit.cover : BoxFit.fitHeight,
+              repeat:
+                  context.isSmall ? ImageRepeat.noRepeat : ImageRepeat.repeat,
+            ),
           ),
           child: Center(
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.end,
               children: [
-                NesRunningText(
-                  text: l10n.leaderboardLabel,
-                  speed: .05,
-                  textStyle: const TextStyle(fontSize: 24),
+                const Spacer(flex: 7),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 32),
+                  child: ConstrainedBox(
+                    constraints: const BoxConstraints(maxWidth: 500),
+                    child: NesContainer(
+                      width: double.infinity,
+                      height: MediaQuery.of(context).size.height / 2,
+                      padding: const EdgeInsets.all(24),
+                      child: Column(
+                        children: [
+                          Text(
+                            l10n.leaderboardLabel,
+                            style: const TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          const SizedBox(height: 24),
+                          BlocBuilder<LeaderboardBloc, LeaderboardState>(
+                            builder: (context, state) => switch (state) {
+                              LeaderboardInitial() => const SizedBox.shrink(),
+                              LeaderboardLoading() =>
+                                const _LeaderboardLoadingWidget(),
+                              LeaderboardError() =>
+                                const LeaderboardErrorWidget(),
+                              LeaderboardLoaded(entries: final entries) =>
+                                _LeaderboardContent(entries: entries),
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
                 ),
-                const SizedBox(height: 24),
-                NesContainer(
-                  width: double.infinity,
-                  height: MediaQuery.of(context).size.height / 2,
-                  child: const Column(),
-                ),
+                const Spacer(),
               ],
             ),
           ),
         ),
       ),
+    );
+  }
+}
+
+class _LeaderboardLoadingWidget extends StatelessWidget {
+  const _LeaderboardLoadingWidget();
+
+  @override
+  Widget build(BuildContext context) {
+    return const Expanded(
+      child: Center(
+        child: NesHourglassLoadingIndicator(),
+      ),
+    );
+  }
+}
+
+class LeaderboardErrorWidget extends StatelessWidget {
+  const LeaderboardErrorWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final l10n = context.l10n;
+    return Expanded(
+      child: Center(
+        child: Text(l10n.errorInputScoreMessage),
+      ),
+    );
+  }
+}
+
+class _LeaderboardContent extends StatelessWidget {
+  const _LeaderboardContent({
+    required this.entries,
+  });
+
+  final List<LeaderboardEntryData> entries;
+
+  @override
+  Widget build(BuildContext context) {
+    final l10n = context.l10n;
+    return Expanded(
+      child: entries.isEmpty
+          ? Center(
+              child: Text(
+                l10n.leaderboardEmptyMessage,
+                textAlign: TextAlign.center,
+              ),
+            )
+          : ListView.builder(
+              itemCount: entries.length,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.only(bottom: 16),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      SizedBox(
+                        width: 60,
+                        child: Text(
+                          '#${index + 1}',
+                          style: TextStyle(
+                            color: switch (index) {
+                              0 => const Color(0xFFD4AF37),
+                              1 => const Color(0xFF9BADB7),
+                              2 => const Color(0xFFCD7F32),
+                              _ => const Color(0xFF1E1E1E),
+                            },
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                      Text(
+                        entries[index].playerInitials,
+                        style: TextStyle(
+                          color: switch (index) {
+                            0 => const Color(0xFFD4AF37),
+                            1 => const Color(0xFF9BADB7),
+                            2 => const Color(0xFFCD7F32),
+                            _ => const Color(0xFF1E1E1E),
+                          },
+                          fontSize: 16,
+                        ),
+                      ),
+                      const SizedBox(width: 5),
+                      Expanded(
+                        child: Text(
+                          entries[index].score.toString(),
+                          style: const TextStyle(
+                            fontSize: 16,
+                          ),
+                          overflow: TextOverflow.ellipsis,
+                          textAlign: TextAlign.right,
+                        ),
+                      ),
+                      const Text(
+                        ' Pts',
+                        style: TextStyle(
+                          fontSize: 16,
+                        ),
+                        textAlign: TextAlign.right,
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
     );
   }
 }
--- lib/game/score/game_over/view/game_over_page.dart
+++ lib/game/score/game_over/view/game_over_page.dart
@@ -4,9 +4,10 @@ import 'package:garda_green/audio/audio.dart';
 import 'package:garda_green/game/menu/menu.dart';
 import 'package:garda_green/game/score/input_initials/view/input_initials_page.dart';
 import 'package:garda_green/game/view/view.dart';
+import 'package:garda_green/gen/assets.gen.dart';
 import 'package:garda_green/l10n/l10n.dart';
 import 'package:garda_green/trivia/trivia.dart';
-import 'package:garda_green/utils/components/components.dart';
+import 'package:garda_green/utils/utils.dart';
 import 'package:nes_ui/nes_ui.dart';
 
 class GameOverPage extends StatefulWidget {
@@ -53,66 +54,87 @@ class _GameOverPageState extends State<GameOverPage> {
           context.read<AudioController>().musicPlayer.setVolume(0.5);
           context.read<AudioController>().changeMusic(Song.background);
         },
-        child: ValueListenableBuilder<bool>(
-          valueListenable: _isClosed,
-          builder: (_, value, __) {
-            return Visibility(
-              visible: value,
-              child: Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    NesRunningText(
-                      text: l10n.gameOverLabel,
-                      speed: .05,
-                      textStyle: const TextStyle(fontSize: 28),
-                    ),
-                    const SizedBox(height: 24),
-                    Text(
-                      l10n.gameScoreLabel(widget.score),
-                      style: const TextStyle(fontSize: 24),
-                    ),
-                    const SizedBox(height: 24),
-                    SizedBox(
-                      width: 150,
-                      child: WobblyButton(
-                        onPressed: () {
-                          Navigator.pushReplacement(
-                            context,
-                            InputInitialsPage.route(
-                              score: widget.score,
+        child: DecoratedBox(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              filterQuality: FilterQuality.high,
+              image: context.isSmall
+                  ? Assets.images.gameOverMobile.provider()
+                  : Assets.images.gameOverDesktop.provider(),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: ValueListenableBuilder<bool>(
+            valueListenable: _isClosed,
+            builder: (_, value, __) {
+              return Visibility(
+                visible: value,
+                child: Center(
+                  child: Align(
+                    alignment: Alignment.bottomCenter,
+                    child: Padding(
+                      padding: const EdgeInsets.only(bottom: 16),
+                      child: NesContainer(
+                        padding: const EdgeInsets.symmetric(horizontal: 48),
+                        height: MediaQuery.of(context).size.height * .42,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            NesRunningText(
+                              text: l10n.gameOverLabel,
+                              speed: .05,
+                              textStyle: const TextStyle(fontSize: 24),
                             ),
-                          );
-                        },
-                        child: Text(
-                          l10n.submitScoreLabel,
-                          textAlign: TextAlign.center,
+                            const SizedBox(height: 16),
+                            Text(
+                              l10n.gameScoreLabel(widget.score),
+                              style: const TextStyle(fontSize: 20),
+                            ),
+                            const SizedBox(height: 24),
+                            SizedBox(
+                              width: 150,
+                              child: WobblyButton(
+                                onPressed: () {
+                                  Navigator.pushReplacement(
+                                    context,
+                                    InputInitialsPage.route(
+                                      score: widget.score,
+                                    ),
+                                  );
+                                },
+                                child: Text(
+                                  l10n.submitScoreLabel,
+                                  textAlign: TextAlign.center,
+                                ),
+                              ),
+                            ),
+                            const SizedBox(height: 16),
+                            SizedBox(
+                              width: 150,
+                              child: WobblyButton(
+                                type: NesButtonType.warning,
+                                onPressed: () => onRestart(context),
+                                child: Text(l10n.retryLabel),
+                              ),
+                            ),
+                            const SizedBox(height: 16),
+                            SizedBox(
+                              width: 150,
+                              child: WobblyButton(
+                                type: NesButtonType.error,
+                                onPressed: () => onExit(context),
+                                child: Text(l10n.exitLabel),
+                              ),
+                            ),
+                          ],
                         ),
                       ),
                     ),
-                    const SizedBox(height: 16),
-                    SizedBox(
-                      width: 150,
-                      child: WobblyButton(
-                        type: NesButtonType.warning,
-                        onPressed: () => onRestart(context),
-                        child: Text(l10n.retryLabel),
-                      ),
-                    ),
-                    const SizedBox(height: 16),
-                    SizedBox(
-                      width: 150,
-                      child: WobblyButton(
-                        type: NesButtonType.error,
-                        onPressed: () => onExit(context),
-                        child: Text(l10n.exitLabel),
-                      ),
-                    ),
-                  ],
+                  ),
                 ),
-              ),
-            );
-          },
+              );
+            },
+          ),
         ),
       ),
     );
